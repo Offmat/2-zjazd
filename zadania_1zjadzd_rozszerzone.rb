de8464daa53ab67c10ef003fa4e91dffccbf1d1e
Mateusz <lesniak.mat@gmail.com>
2017-10-27 08:12:56 +0200

    Nic ciekawego jeszcze tu nie ma

--- zadania_1zjadzd_rozszerzone.rb
+++ zadania_1zjadzd_rozszerzone.rb
@@ -33,3 +33,36 @@ require "pry"
 # rescue ZeroDivisionError
 #   puts "to się nie uda"
 # end
+
+
+
+
+
+
+#czytanie z plików
+
+# contents = File.read("/etc/hosts")
+# puts contents
+
+def longest_line(file_name)
+  file = File.open(file_name)
+  longest = ""
+  file.each do |line|
+    longest = line if line.length > longest.length
+  end
+  file.close
+  longest
+end
+
+
+# "./code\ sensei/kurs\ stacjonarny/zadania_2/testowy.txt"
+# "~/code\ sensei/kurs\ stacjonarny/zadania_2/testowy.txt"
+#"/home/mateusz/code\ sensei/kurs\ stacjonarny/zadania_2/testowy.txt"
+#czemu to nie działa z pliku a z terminala działa? jak się odwoływać do adresów bezwzględnych z pliku? zwłaszcza, że do hosts działa też od "/" i adres. Mam problem ze strukturą systemu chyba trochę - jakieś pomocne linki, materiały?
+
+
+puts longest_line("/etc/hosts")
+puts longest_line("testowy.txt")  #na szczęście są jeszcze adresy względne
+
+data = File.open("testowy.txt") {  |line| line.reverse } #skąd tu się wzięły dodatkowe /n przed każdą linią?!
+data.each {|l| puts l.inspect}
